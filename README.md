7fb06a86822694dd75ba1435e7e833003950501f
ivanchc <ivanchanny1997@gmail.com>
2019-04-13 19:23:15 +0100

    Set up of the git repo.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dynamics
+Simulation of dynamic systems using numerical methods, the results is visualised by animation and other forms of graphical representation.
+
+## Simple Pendulum
+<p> This is a simulation of a pendulum using ordinary differenctial equation derived from the Lagrangian mechanics.<br/>
+The motion of the pendulum can be descibed using a 2nd order differential equation:<br/>
+</p>
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='dynamics',
+      version='0.0.1',
+      url='https://https://github.com/IvanCHC/Dynamics.git',
+      author='Chon-Hou Ivan Chan',
+      author_email='ich.chan26@gmail.com',
+      license='LICENSE',
+      keywords='simulation',
+      package =[
+          '',
+      ],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib',      
+      ],
+    )
